0ca1d2702b85eb968dcd1ce6e1a3818366dc0c88
kmitprojectschool <sireesha@kmit.in>
2023-02-15 16:36:18 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MyGrid from './components/MyGrid';
+
+function App() {
+  return (
+    <div className="App">
+     <MyGrid/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MyGrid.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import one from './img-local/1.jpg'  // image path is relative to this file//
+
+const MyGrid = () => {
+    return (
+        <div>
+            
+<div className="container">
+    <div className="row">
+        <div className="col-sm-4">           
+              <img className="img-thumbnail" src={one}/> 
+        </div>
+        <div className="col-sm-4">
+            <img className="img-thumbnail" src="./img-local/2.jpg"/>
+        </div>
+        <div className="col-sm-4">
+            <img className="img-thumbnail" src="img-local/3.jpg"/>
+        </div>
+    </div> 
+
+    <div className="row">
+        <div className="col-sm-4">
+            <img className="img-thumbnail" src="imgs/4.jpg"/>
+        </div>
+        <div className="col-sm-4">
+            <img className="img-thumbnail" src="imgs/5.jpg"/>
+        </div>
+        <div className="col-sm-4">
+            <img className="img-thumbnail" src="imgs/6.jpg"/>
+        </div>
+    </div> 
+
+
+</div>
+            
+        </div>
+    );
+};
+
+export default MyGrid;
